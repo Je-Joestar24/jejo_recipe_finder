e7422557338da497dcd8e5f5b8906bc33db7848e
Jejomar Parrilla <jpar1252003@gmail.com>
2025-07-30 23:17:53 +0800

    ref: Removed the static files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
-  viteConfig,
   defineConfig({
     test: {
       environment: 'jsdom',
