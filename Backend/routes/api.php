3e63ac6d7c6d01a4abfbb2b6935296e74651ce3d
Jejomar Parrilla <jpar1252003@gmail.com>
2025-09-12 01:14:07 +0800

    Backend: feat(sanctum): Fixed the sanctum touched guards

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -8,4 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/register', [RegisterUserController::class, 'store']);
     Route::post('/login', [LoginController::class, 'login']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [LoginController::class, 'logout']);
+        Route::get('/me', function () {
+            return response()->json(app()->version());
+        });
+    });
 });
