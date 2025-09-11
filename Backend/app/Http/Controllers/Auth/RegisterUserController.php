054ffa4497a1d04b7e1baa54fee02e35fcad3db4
Jejomar Parrilla <jpar1252003@gmail.com>
2025-09-11 23:48:19 +0800

    Backend: Created the Request for User Registration

--- Backend/app/Http/Controllers/Auth/RegisterUserController.php
+++ Backend/app/Http/Controllers/Auth/RegisterUserController.php
@@ -1,11 +1,31 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
+use App\Http\Resources\UserResource;
+use Illuminate\Database\QueryException;
 
 class RegisterUserController extends Controller
 {
-    //
+    /**
+     * Handle user signup and return JSON response.
+     */
+    public function store(RegisterUserRequest $request)
+    {
+        $data = $request->validated();
+
+        try {
+            $user = User::create($data);
+
+            return new UserResource($user);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Could not create user.',
+            ], 500);
+        }
+    }
 }
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\auth\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-    return response()->json(app()->version());
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [RegisterUserController::class, 'store']);
 });
