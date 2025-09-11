e57a7087554b88d79ecaff9df540de31ff3ed24d
Jejomar Parrilla <jpar1252003@gmail.com>
2025-09-11 15:11:41 +0800

    Backend: feat(cors): Setup to accept requests from the frontend

---
+++ Frontend/src/config/api.ts
@@ -0,0 +1,50 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000',
+    withCredentials: true,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+    },
+})
+
+export async function csrf() {
+    try {
+        const response = await api.get('/sanctum/csrf-cookie')
+
+        await new Promise(resolve => setTimeout(resolve, 100))
+
+        const token = getCookie('XSRF-TOKEN')
+
+        if (token) {
+            const decodedToken = decodeURIComponent(token)
+
+            api.defaults.headers.common['X-CSRF-TOKEN'] = decodedToken
+        } else {
+            console.warn('No CSRF token found in cookies')
+        }
+
+        return true
+    } catch (error) {
+        return false
+    }
+}
+
+function getCookie(name: string): string | null {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+  
+    if (parts.length === 2) {
+      const lastPart = parts.pop(); // type: string | undefined
+      if (lastPart) {
+        return lastPart.split(";").shift() ?? null;
+      }
+    }
+  
+    return null;
+  }
+  
+
+export default api
