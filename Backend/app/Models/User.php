2ee9d27f6768d522e46a63c3297f80ea0d21a1de
Jejomar Parrilla <jpar1252003@gmail.com>
2025-09-17 00:47:38 +0800

    Backend: feat(model): User model altered

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -46,4 +46,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+
+    public function favoriteRecipes()
+    {
+        return $this->belongsToMany(Recipe::class, 'favorites');
+    }
 }
