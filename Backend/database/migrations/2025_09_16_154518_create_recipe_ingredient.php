0827edacf247685d7b53a9e6dad7f1704437228c
Jejomar Parrilla <jpar1252003@gmail.com>
2025-09-16 23:46:36 +0800

    Backend: feat(schema): Created the recipe_ingredient table migration to connect recipe and ingredients

---
+++ Backend/database/migrations/2025_09_16_154518_create_recipe_ingredient.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipe_ingredient', function (Blueprint $table) {
+            $table->id(); // PK
+            $table->foreignId('recipe_id')
+                ->constrained('recipes')
+                ->cascadeOnDelete();
+            $table->foreignId('ingredient_id')
+                ->constrained('ingredients');
+            $table->decimal('amount', 10, 2)->nullable();
+            $table->string('unit', 50)->nullable();
+
+            $table->unique(['recipe_id', 'ingredient_id']); // 1 ingredient per recipe
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipe_ingredient');
+    }
+};
